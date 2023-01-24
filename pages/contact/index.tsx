6e0f57f55b3d3d3548767a58fa46bb42cc31b42c
Utsav <mail.utsav02@gmail.com>
2023-01-24 18:45:04 +0530

    routes added

---
+++ pages/contact/index.tsx
@@ -0,0 +1,12 @@
+import Link from 'next/link'
+
+export default function About() {
+  return (
+    <div>
+      <div>Contact 2</div>
+      <div>
+        Back to <Link href="/">Home</Link>
+      </div>
+    </div>
+  )
+}
